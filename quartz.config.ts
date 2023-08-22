9113a1f6b5ec3275f3ba2b4e8b77a5ee27f64902
jandrefrey <94923840+jandrefrey@users.noreply.github.com>
2023-08-22 09:00:38 +0200

    Configs

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Jandre's Notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "jandrefrey.github.io/quartz",
     ignorePatterns: ["private", "templates"],
     theme: {
       typography: {
